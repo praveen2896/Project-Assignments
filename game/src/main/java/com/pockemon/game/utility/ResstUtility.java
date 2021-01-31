c6024a30b40cc555f64a1cb53f4dbb94e1e34758
PraveenKumar <mpraveenkumarown@gmail.com>
2021-01-31 16:43:52 +0530

    add my user agent

--- game/src/main/java/com/pockemon/game/utility/ResstUtility.java
+++ game/src/main/java/com/pockemon/game/utility/ResstUtility.java
@@ -20,7 +20,7 @@ public class ResstUtility {
 		ResponseEntity<String> finalResponse=null;
 		ResponseEntity<String> response1=null;
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 RuxitSynthetic/1.0 v8424762251 t38550 ath9b965f92 altpub cvcv=2");
+        headers.add("user-agent", "spring rest api");
         HttpEntity<String> entity = new HttpEntity<String>("parameters", headers); 
         try {
         response1 = restTemplate.exchange(url, HttpMethod.GET,entity,String.class);
@@ -35,6 +35,7 @@ public class ResstUtility {
         catch(Exception e)
         {
         	System.out.println("Exception"+e.getMessage());
+            System.out.println("Ex"+e.getCause());
         	finalResponse=null;
         }
         return finalResponse;
